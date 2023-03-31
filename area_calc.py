44654fb43152b1dd906cce1e50a8dfec1008fed5
Patryk <patrykokpara@hotmail.com>
2023-03-31 18:59:12 +0100

    Created python program that calculates the sq area

--- area_calc.py
+++ area_calc.py
@@ -2,3 +2,11 @@
 # name = input('Type in your name')
 # age = input('and your age??')
 # print(name, "you are", age)
+
+#calc the area od the circle
+
+radius = input('Enter the radius of your circle (m):')
+# typecasting
+area = 3.142* int(radius) ** 2
+
+print('The area of your circle is:', area)
