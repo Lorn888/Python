f8a122e8f1ff32e181fdc2dbb53beb0f8f85f285
Patryk <patrykokpara@hotmail.com>
2023-03-31 19:09:01 +0100

    Precision method

--- lessons/string_format.py
+++ lessons/string_format.py
@@ -5,4 +5,4 @@ num2 = 10.2342342
 # print('num1 is', num1, 'and num2 is', num2)
 
 #FORMAT METHOD
-print('num1 is {0} and num2 is {1}'.format(num1, num2))
+print('num1 is {0:.3} and num2 is {1:.3}'.format(num1, num2))
