5b632b745ceff060c2341423b2d4f1157882099d
Patryk <patrykokpara@hotmail.com>
2023-04-01 18:54:10 +0100

    Functions

---
+++ lessons/functions.py
@@ -0,0 +1,7 @@
+def greet(name = 'NAME', time = 'TIME'):
+    print(f'Good {time} {name}, hope u good')
+
+# time = input('Whats the time of the day')
+# name = input('Whats your name')
+
+greet(time='afternoon')
